aa21581f31c29385bccb25d0dae1f37166b99238
Shiro Han <shan99business@gmail.com>
2020-05-05 18:31:35 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -5,9 +5,9 @@ def say_hello_world_ten_times
   puts phrase
   puts phrase
   puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 puts phrase
 puts phrase
 end
